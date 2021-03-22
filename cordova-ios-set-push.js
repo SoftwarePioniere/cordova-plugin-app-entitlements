dc1888858f1ae26b3b96fc14714168ab5c610ca4
Tobias Böker <tboeker@users.noreply.github.com>
2021-03-22 09:37:40 +0100

    Update cordova-ios-set-push.js

--- cordova-ios-set-push.js
+++ cordova-ios-set-push.js
@@ -5,7 +5,6 @@
  */
 
 
-
 module.exports = function (context)
 {
       if (!context.opts.platforms || !context.opts.platforms.includes('ios')) {
@@ -48,3 +47,48 @@ module.exports = function (context)
     
       return deferral.promise;
 };
+
+/**
+module.exports = function (context)
+{
+      if (!context.opts.platforms || !context.opts.platforms.includes('ios')) {
+          return;
+      }
+        
+      var deferral = require('q').defer();
+    
+      var fs = require('fs');
+      var xcode = require('xcode');
+      var path = require('path');
+      var common = require('cordova-common');
+      
+      var rootPath = context.opts.projectRoot;
+      var configXmlPath = path.join(rootPath, 'config.xml');
+      
+      var configParser = new common.ConfigParser(configXmlPath);
+      var appName = configParser.name();
+        
+      var projectPath = './platforms/ios/' + appName + '.xcodeproj/project.pbxproj';
+      var project = xcode.project(projectPath);
+
+      project.parse(function(err) {
+        if (err) {
+          console.error('cordova-ios-set-push::error');
+          console.error(err);
+          deferral.reject('xcode could not parse project');
+          
+        } else{
+        
+          console.log('cordova-ios-set-push::writing SystemCapabilities');
+          
+          var pushEntitlement = "{com.apple.Push ={enabled = 1;};}";
+          project.addTargetAttribute("SystemCapabilities", pushEntitlement);
+
+          fs.writeFileSync(projectPath, project.writeSync());
+          deferral.resolve();
+        }
+      });
+    
+      return deferral.promise;
+};
+*/
